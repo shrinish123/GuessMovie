f510bdbb04b24f3b958933d8ce13d7510fbcae23
shrinish123 <shrinishiitkgp2001@gmail.com>
2020-12-10 19:32:19 +0530

    Added Start scripts and nodemon

--- src/utils/randomMovie.js
+++ src/utils/randomMovie.js
@@ -1,11 +1,60 @@
 const request = require('postman-request')
 
-const randomMovie = (id,callback) =>{
-
-      
-    const url = 'http://www.omdbapi.com/?i='+ encodeURIComponent(movieName) + '&apikey=74648ab2'
-
-
-    
+const pad =(number,length) =>{
+       
+    var str =''+number;
+    while(str.length<length){
+        str='0' +str;
+    }
+    return str;
 }
- 
+
+
+const randomMovie = (callback) =>{
+
+    var id = pad(Math.floor((Math.random()*2155529) +1),7);
+    const url = 'http://www.omdbapi.com/?i=tt'+ id + '&apikey=74648ab2'
+
+    request({ url:url,json:true}, (err,res) =>{  
+        if(err){
+          callback('Unable to connect to the server',undefined)
+        }
+    
+        const movieInfo = res.body;
+        callback(undefined,movieInfo);
+      
+    })
+}
+
+// const movieGenerate= ()=>{
+
+//     let plot = randomMovie((err,res)=>{
+//         if(err){
+//             return  console.log('unable to find a movie')
+//          }
+//          return res.Plot;
+//     })
+//     console.log(plot)
+
+//    while(plot ==='N/A') {
+        
+//        plot = randomMovie((err,res)=>{
+//         if(err){
+//             return  console.log('unable to find a movie')
+//          }
+//          return res.Plot;
+//     })
+//    }
+//    console.log(plot);
+// }
+
+//  movieGenerate();
+
+let plot = randomMovie((err,res)=>{
+            // if(err){
+            //     return  console.log('unable to find a movie')
+            //  }
+             return res.Plot ;
+        })
+
+       console.log(plot) 
